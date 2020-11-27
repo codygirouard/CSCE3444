62be74fa2cb4647918a7e24b87bf18a05ab8a330
Cody Girouard <codyagirouard@gmail.com>
2020-11-27 16:03:18 -0600

    New Branch, All Items Added
    All items added and owner functionality implemented. Needs to be connected with database still.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,30 +24,30 @@ namespace WpfApp1
         {
             get; private set;
         }
-        
+
+        string[] tools = { "Screwdriver", "Hammer", "Plier", "Wrench", "Ladder", "Clamp", "Tape Measure", "Tool Belt", "Axe", "Chisel", "Drill", "Saw", "Sander", "Air Compressor", "Staple Gun", "Nail", "Screw", "Shovel", "Caliper", "Ruler" };
+        int[] stocks = { 13, 6, 0, 7, 22, 15, 1, 20, 18, 19, 10, 2, 0, 14, 44, 26, 7, 16, 19, 20 };
+        double[] prices = { 12.99, 21.50, 16.00, 15.99, 9.99, 22.49, 30.00, 23.99, 6.50, 18.99, 19.99, 33.33, 27.89, 22.22, 9.99, 0.50, 0.50, 20.00, 4.99, 1.99 };
+        string[] descs = { "A screwdriver is a tool, manual or powered, used for screwing and unscrewing screws.", "A hammer is a tool consisting of a weighted \"head\" fixed to a long handle that is swung to deliver an impact to a small area of an object.", "Pliers are a hand tool used to hold objects firmly.", "A wrench or spanner is a tool used to provide grip and mechanical advantage in applying torque to turn objects—usually rotary fasteners, such as nuts and bolts—or keep them from turning.", "A ladder is a vertical or inclined set of rungs or steps.", "A clamp is a fastening device used to hold or secure objects tightly together to prevent movement or separation through the application of inward pressure.", "A tape measure or measuring tape is a flexible ruler used to measure size or distance.", "A belt for carrying tools; a utility belt.", "An axe is an implement that has been used for millennia to shape, split and cut wood, to harvest timber, as a weapon, and as a ceremonial or heraldic symbol.", "A chisel is a tool with a characteristically shaped cutting edge of blade on its end, for carving or cutting a hard material such as wood, stone, or metal by hand, struck with a mallet, or mechanical power.", "A drill or drilling machine is a tool primarily used for making round holes or driving fasteners.", "A saw is a tool consisting of a tough blade, wire, or chain with a hard toothed edge.", "A sander is a power tool used to smooth surfaces by abrasion with sandpaper.", "An air compressor is a pneumatic device that converts power into potential energy stored in pressurized air (i.e., compressed air).", "A staple gun or powered stapler is a hand-held machine used to drive heavy metal staples into wood, plastic, or masonry.", "A nail is a small object made of metal which is used as a fastener, as a peg to hang something, or sometimes as a decoration.", "A screw and a bolt are similar types of fastener typically made of metal, and characterized by a helical ridge, known as a male thread.", "A shovel is a tool for digging, lifting, and moving bulk materials, such as soil, coal, gravel, snow, sand, or ore.", "A caliper is a device used to measure the dimensions of an object.", "A ruler, sometimes called a rule or line gauge, is a device used in geometry and technical drawing, as well as the engineering and construction industries, to measure distances or draw straight lines." };
+        int item = 0;
         public MainWindow()
         {
             InitializeComponent();
             admin = false;
-            Tool tempTool = new Tool();
-        }
-        public void UpdateToolInfoPage(string nameOfTool)  // function to access database and update ToolsInfo page
-        {
-            // getting the data from the database
-            DataAccess db = new DataAccess();
-            Tool obj = db.GetInfo(nameOfTool);
-
-            // Update textboxes
-            DescriptionBox.Text = obj.Description;          
-            Name.Text = obj.Name;
-            Price.Text = "$" + obj.Price.ToString();
-            StockNum.Text = obj.Quantity.ToString();
         }
 
         private void CkInven_Click(object sender, RoutedEventArgs e)
         {
             this.Welcome.Visibility = Visibility.Hidden;
             this.Inventory.Visibility = Visibility.Visible;
+            this.txt1.Text = tools[0];
+            this.img1.Source = new BitmapImage(new Uri("Pictures/" + tools[0] + ".png", UriKind.Relative)); 
+            this.txt2.Text = tools[1];
+            this.img2.Source = new BitmapImage(new Uri("Pictures/" + tools[1] + ".png", UriKind.Relative));
+            this.txt3.Text = tools[2];
+            this.img3.Source = new BitmapImage(new Uri("Pictures/" + tools[2] + ".png", UriKind.Relative));
+            this.txt4.Text = tools[3];
+            this.img4.Source = new BitmapImage(new Uri("Pictures/" + tools[3] + ".png", UriKind.Relative));
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
@@ -149,67 +148,124 @@ namespace WpfApp1
                 this.Password.Focus();
             }
         }
+
         private void Forward_Click(object sender, RoutedEventArgs e)
         {
-            this.Inventory.Visibility = Visibility.Hidden;
-            this.Inventory2.Visibility = Visibility.Visible;
+            if (this.PageNum.Text == "5")
+            {
+                MessageBox.Show("This is the last page.", "No Pages Left");
+                return;
+            }
+            int currPage = Int16.Parse(this.PageNum.Text);
+            currPage++;
+            this.PageNum.Text = currPage.ToString();
+            this.txt1.Text = tools[0 + (currPage - 1) * 4];
+            this.img1.Source = new BitmapImage(new Uri("Pictures/" + tools[0 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+            this.txt2.Text = tools[1 + (currPage - 1) * 4];
+            this.img2.Source = new BitmapImage(new Uri("Pictures/" + tools[1 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+            this.txt3.Text = tools[2 + (currPage - 1) * 4];
+            this.img3.Source = new BitmapImage(new Uri("Pictures/" + tools[2 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+            this.txt4.Text = tools[3 + (currPage - 1) * 4];
+            this.img4.Source = new BitmapImage(new Uri("Pictures/" + tools[3 + (currPage - 1) * 4] + ".png", UriKind.Relative));
         }
-        private void Back1_Click(object sender, RoutedEventArgs e)
+
+        private void Back_Click(object sender, RoutedEventArgs e)
         {
-            this.Inventory2.Visibility = Visibility.Hidden;
+            if (this.PageNum.Text == "1")
+            {
+                MessageBox.Show("This is the first page.", "Cannot Go Back");
+                return;
+            }
+            int currPage = Int16.Parse(this.PageNum.Text);
+            currPage--;
+            this.PageNum.Text = currPage.ToString();
+            this.txt1.Text = tools[0 + (currPage - 1) * 4];
+            this.img1.Source = new BitmapImage(new Uri("Pictures/" + tools[0 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+            this.txt2.Text = tools[1 + (currPage - 1) * 4];
+            this.img2.Source = new BitmapImage(new Uri("Pictures/" + tools[1 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+            this.txt3.Text = tools[2 + (currPage - 1) * 4];
+            this.img3.Source = new BitmapImage(new Uri("Pictures/" + tools[2 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+            this.txt4.Text = tools[3 + (currPage - 1) * 4];
+            this.img4.Source = new BitmapImage(new Uri("Pictures/" + tools[3 + (currPage - 1) * 4] + ".png", UriKind.Relative));
+        }
+
+        private void Item_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = e.Source as Button;
+            int offset = 0;
+            switch (b.Name)
+            {
+                case "Button1":
+                    offset = 0;
+                    break;
+                case "Button2":
+                    offset = 1;
+                    break;
+                case "Button3":
+                    offset = 2;
+                    break;
+                case "Button4":
+                    offset = 3;
+                    break;
+                default:
+                    MessageBox.Show("Error Button Not Found");
+                    break;
+            }
+            item = offset + 4 * (Int16.Parse(this.PageNum.Text) - 1);
+            this.Price.Text = "$" + String.Format("{0:0.00}", prices[item]);
+            this.Stock.Text = "In Stock: " + stocks[item].ToString();
+            this.txt.Text = tools[item];
+            this.Desc.Text = descs[item];
+            this.StockText.Text = stocks[item].ToString();
+            this.img.Source = new BitmapImage(new Uri("Pictures/" + tools[item] + ".png", UriKind.Relative));
+            if (admin)
+            {
+                this.OwnView.Visibility = Visibility.Visible;
+                this.CustView.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                this.OwnView.Visibility = Visibility.Hidden;
+                this.CustView.Visibility = Visibility.Visible;
+            }
+            this.Inventory.Visibility = Visibility.Hidden;
+            this.Item.Visibility = Visibility.Visible;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
             this.Inventory.Visibility = Visibility.Visible;
-        }
-        private void LeaveScrewdriver_Click(object sender, RoutedEventArgs e) // goes back to inventory page from Screwdrive Info page
-        {
-            this.ToolsInfo.Visibility = Visibility.Hidden;
-            this.Inventory.Visibility = Visibility.Visible;
-            
+            this.Item.Visibility = Visibility.Hidden;
         }
 
-        private void ScrewdriverInfo_Click(object sender, RoutedEventArgs e) // goes to screwdriver Information
+        private void PurchaseButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Inventory.Visibility = Visibility.Hidden;
-            this.ToolsInfo.Visibility = Visibility.Visible;
-            UpdateToolInfoPage("Screwdriver");    
-        }
-        private void HammerInfo_Click(object sender, RoutedEventArgs e)
-        {
-            this.Inventory.Visibility = Visibility.Hidden;
-            this.ToolsInfo.Visibility = Visibility.Visible;
-            UpdateToolInfoPage("Hammer");
-        }
-        private void PliersInfo_Click(object sender, RoutedEventArgs e)
-        {
-            this.Inventory.Visibility = Visibility.Hidden;
-            this.ToolsInfo.Visibility = Visibility.Visible;
-            UpdateToolInfoPage("Pliers");
-        }
-        private void WrenchInfo_Click(object sender, RoutedEventArgs e)
-        {
-            this.Inventory.Visibility = Visibility.Hidden;
-            this.ToolsInfo.Visibility = Visibility.Visible;
-            UpdateToolInfoPage("Wrench");
+            if (stocks[item] < 1)
+            {
+                MessageBox.Show("Out of Stock", "No Inventory Left");
+                return;
+            }
+            stocks[item]--;
+            this.Stock.Text = "In Stock: " + stocks[item].ToString();
         }
 
-
-        private void PurchaseSD_Click(object sender, RoutedEventArgs e) // purchase stock button
+        private void StockButton_Click(object sender, RoutedEventArgs e)
         {
+            int stock = 0;
+            bool isNum = int.TryParse(this.StockText.Text, out stock);
+            if (!isNum)
+            {
+                MessageBox.Show("Not a number entry", "Try Again");
+                return;
+            }
+            if (stock < 0)
+            {
+                MessageBox.Show("Please enter a number equal to or greater than 0", "Incorrect Number Entry");
+                return;
+            }
 
-        }
-
-        private void Item_Description_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-           
+            stocks[item] = stock;
+            this.Stock.Text = "In Stock: " + stocks[item].ToString();
         }
     }
 }
